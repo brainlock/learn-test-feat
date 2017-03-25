ed3c09a5c6db437789bbb9d3344cbb652be03d2d
Alberto Piai <alberto.piai@gmail.com>
2017-03-25 11:49:35 +0100

    more tests

--- __tests__/finite.js
+++ __tests__/finite.js
@@ -86,3 +86,73 @@ test('index of singleton', () => {
         expect(() => nth(foo, i)).toThrow();
     }
 });
+
+test('index of sum', () => {
+    const foo = singleton('foo');
+    const bar = singleton('bar');
+    const baz = singleton('baz');
+
+    const sum1 = sum(sum(foo, bar), baz);
+    const sum2 = sum(foo, sum(bar, baz));
+
+    for (const s of [sum1, sum2]) {
+        expect(nth(s, 0)).toEqual('foo');
+        expect(nth(s, 1)).toEqual('bar');
+        expect(nth(s, 2)).toEqual('baz');
+
+        for (let i = 3; i < 100; i++) {
+            expect(() => nth(s, i)).toThrow();
+        }
+    }
+});
+
+test('index of product', () => {
+    const foo = singleton('foo');
+    const bar = singleton('bar');
+
+    const prod = product(sum(foo, bar), sum(foo, bar));
+
+    expect(nth(prod, 0)[0]).toEqual('foo');
+    expect(nth(prod, 0)[1]).toEqual('foo');
+    expect(nth(prod, 1)[0]).toEqual('foo');
+    expect(nth(prod, 1)[1]).toEqual('bar');
+    expect(nth(prod, 2)[0]).toEqual('bar');
+    expect(nth(prod, 2)[1]).toEqual('foo');
+    expect(nth(prod, 3)[0]).toEqual('bar');
+    expect(nth(prod, 3)[1]).toEqual('bar');
+
+    for (let i = 4; i < 100; i++) {
+        expect(() => nth(prod, i)).toThrow();
+    }
+});
+
+test('index of map', () => {
+    const fin = [1, 2, 3].map(singleton).reduce(sum);
+
+    const finDoubled = map(n => n * 2, fin);
+
+    expect(nth(finDoubled, 0)).toEqual(2);
+    expect(nth(finDoubled, 1)).toEqual(4);
+    expect(nth(finDoubled, 2)).toEqual(6);
+
+    for (let i = 3; i < 100; i++) {
+        expect(() => nth(finDoubled, i)).toThrow();
+    }
+});
+
+test('index of apply', () => {
+    const fs = [n => n * 2, n => n * 3].map(singleton).reduce(sum);
+
+    const xs = [1, 2].map(singleton).reduce(sum);
+
+    const applied = apply(fs, xs);
+
+    expect(nth(applied, 0)).toBe(2);
+    expect(nth(applied, 1)).toBe(4);
+    expect(nth(applied, 2)).toBe(3);
+    expect(nth(applied, 3)).toBe(6);
+
+    for (let i = 4; i < 100; i++) {
+        expect(() => nth(applied, i)).toThrow();
+    }
+});
